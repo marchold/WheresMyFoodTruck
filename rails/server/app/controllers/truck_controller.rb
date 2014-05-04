e2ea4777d22baec05dbffbda2b04930e9fda0d88
marchold <goblets@gmail.com>
2014-05-04 14:45:58 -0700

    WIP

--- rails/server/app/controllers/truck_controller.rb
+++ rails/server/app/controllers/truck_controller.rb
@@ -1,9 +1,16 @@
+require 'securerandom'
+
 class TruckController < ApplicationController
 
   def update
-      render :json => {:haha => params[:truck_id],
+     #TODO:
+     #Validate that the token against the truck_id on the OpenTrucks table 
+     #then write the longitute and latitude in to the table
+     #we can return a simple result=>success result 
+     render :json => {:haha => params[:truck_id],
                        :hoh  => params[:lng],
-                       :hack => params[:lat]
+                       :hack => params[:lat],
+		       :do => params[:token]
                       } 
   end
 
@@ -13,7 +20,11 @@ class TruckController < ApplicationController
       #      then if the truck is not already open and the user had permission to
       #      open the truck, we create an entry in the OpenTrucks table and create
       #      an access token that is written to that table, and return the token
-      render :json => {:token => 'woah'}
+      random_string = SecureRandom.urlsafe_base64(64)
+      userId = params[:userId]
+      truckId = params[:truck_id]
+      
+      render :json => {:token => random_string, :truck_id => params[:truck_id], :user_id => params[:user_id]}
   end
 
 end
