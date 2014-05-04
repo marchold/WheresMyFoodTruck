3767e9e51b9c019cc522872a46cca17f1c180850
marchold <goblets@gmail.com>
2014-05-04 16:55:37 -0700

    Got the basic logic for a user to mark a truck as open by creating an entry in the opentrucks table and get an access token back to the user so the location can be tracked.

--- rails/server/app/controllers/truck_controller.rb
+++ rails/server/app/controllers/truck_controller.rb
@@ -16,15 +16,38 @@ class TruckController < ApplicationController
 
 
   def show
+
       #TODO: we have to look up the user and make sure he has the requested truck
       #      then if the truck is not already open and the user had permission to
       #      open the truck, we create an entry in the OpenTrucks table and create
       #      an access token that is written to that table, and return the token
-      random_string = SecureRandom.urlsafe_base64(64)
-      userId = params[:userId]
-      truckId = params[:truck_id]
+
+
       
-      render :json => {:token => random_string, :truck_id => params[:truck_id], :user_id => params[:user_id]}
+      user = User.find_by_id(params[:user_id])
+      if (user != nil) 
+
+      	 truck = user.trucks.find_by_id(params[:truck_id])            
+      	 if (truck != nil)
+
+	     if (nil == (OpenTruck.find_by truck_id: truck.id))	    
+
+	       #At this point we know the truck is not already open and we can go ahead and assign a token
+               token = SecureRandom.urlsafe_base64(64)
+	       OpenTruck.create(:truck_id => truck.id, :token => token)
+
+	       render :json => { :token => token }
+	    	    
+             else
+	       render :json => "Truck is already open"
+             end
+
+	 else
+	    render :json => "invalid truck"
+	 end
+      else
+	render :json => "invalid user"
+      end
   end
 
 end
